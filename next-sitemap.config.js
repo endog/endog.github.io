f890dcdca375f79e9b72116c019e83be6cf0f8c4
endog <yoh.12356@gmail.com>
2024-05-18 22:55:36 +0900

    sample fix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,8 @@
 module.exports = {
     siteUrl: 'https://endog.github.io', // 自分のサイトのURLに変更してください
     generateRobotsTxt: true, // robots.txt ファイルも生成する場合
+    changefreq: 'yearly',
+    excludeIndex: true,
+    sitemapSize: 5000,
     // その他のオプションを必要に応じて追加できます
   }
